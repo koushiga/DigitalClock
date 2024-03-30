e812a1447ccb5521ccd59af311df8a7a38781ec5
koushiga <koushigakannan99@gmail.com>
2024-03-30 23:39:46 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Digital from './DigitalClock';
+
+function App() {
+  return (
+    <>
+        <Digital/>      
+    </>
+  ); 
+}
+
+export default App;
